fcc223af3ac4bc8df8b34618a232083c24ef29d3
t1chit <temur.chitashvili@space.ge>
2025-05-27 13:38:35 +0400

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Temur Chitashvili on 27.05.25.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
